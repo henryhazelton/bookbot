e5133e8cdb1e8b9498ef7a9c5b3f5322b3eecd79
henryhazelton <henryhazelton00@gmail.com>
2023-08-31 14:43:45 +0100

    Added a sorting dictionary function that returns a list of the alphabetical characters in the book text, in the form of a tuple showing the character and the number of times that character appears in the text.

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ def main():
     book_text = read_the_book(book_path)
     print(book_text)
     number_of_words = count_words(book_text)
-    print(f"there are {number_of_words} words in this book.")
+    print(f"There are {number_of_words} words in this book.")
     number_of_letters = count_letters(book_text)
     print(number_of_letters)
+    list = sort_dictionary(book_text)
+    print(list)
+
+def construct_report(book_text):
+    character_list = sort_dictionary(book_text)
+
+
+def sort_dictionary(book_text):
+    unsorted_dict = count_letters(book_text)
+    unsorted_list = list(unsorted_dict.items())
+    alpha_unsorted_list = []
+    for item in unsorted_list:
+        if item[0].isalpha():
+            alpha_unsorted_list.append(item)
+    alpha_sorted_list = sorted(alpha_unsorted_list)
+    return alpha_sorted_list
 
 def count_letters(book_text):
     character_dict = {}
