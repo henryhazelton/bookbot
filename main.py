680edf68d167667b82b22024d72f823a7ba1a039
henryhazelton <henryhazelton00@gmail.com>
2023-09-01 09:55:50 +0100

    Added a function to construct the report which tells me how many times each character appears in the text.

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ def main():
     print(number_of_letters)
     list = sort_dictionary(book_text)
     print(list)
+    construct_report(book_path, book_text)
+    
 
-def construct_report(book_text):
+def construct_report(book_path, book_text):
     character_list = sort_dictionary(book_text)
-
+    print(f"--- Begin report of {book_path} ---")
+    for character in character_list:
+        print(f"The {character[0]} character was found {character[1]} times.")
+    print("--- End report ---")
 
 def sort_dictionary(book_text):
     unsorted_dict = count_letters(book_text)
