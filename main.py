d324492403de79288ed4f925d8f7efde25f10dae
henryhazelton <henryhazelton00@gmail.com>
2025-04-02 13:32:18 +0100

    moved count letters function to stats.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from stats import count_words
+from stats import count_letters
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = read_the_book(book_path)
     print(book_text)
     number_of_words = count_words(book_text)
@@ -30,15 +35,7 @@ def sort_dictionary(book_text):
     alpha_sorted_list = sorted(alpha_unsorted_list)
     return alpha_sorted_list
 
-def count_letters(book_text):
-    character_dict = {}
-    low_book_text = book_text.lower()
-    for character in low_book_text:
-        if character not in character_dict:
-            character_dict[character] = 1
-        else:
-            character_dict[character] += 1
-    return character_dict
+
 
 
 def  read_the_book(book):
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def count_words(book_text):
     words_in_text = book_text.split()
     return len(words_in_text)
+
+def count_letters(book_text):
+    character_dict = {}
+    low_book_text = book_text.lower()
+    for character in low_book_text:
+        if character not in character_dict:
+            character_dict[character] = 1
+        else:
+            character_dict[character] += 1
+    return character_dict
