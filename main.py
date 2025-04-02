99b825e583570f982e4f5227fb5f0f9eec57ce17
henryhazelton <henryhazelton00@gmail.com>
2025-04-02 13:43:48 +0100

    updated sorting function and removed print statements

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from stats import count_letters
 import sys
 
 def main():
-
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = read_the_book(book_path)
-    print(book_text)
     number_of_words = count_words(book_text)
     print(f"{number_of_words} words found in the document")
     number_of_letters = count_letters(book_text)
-    print(number_of_letters)
+
     list = sort_dictionary(book_text)
-    print(list)
     construct_report(book_path, book_text)
     
 
@@ -19,7 +20,7 @@ def construct_report(book_path, book_text):
     character_list = sort_dictionary(book_text)
     print(f"--- Begin report of {book_path} ---")
     for character in character_list:
-        print(f"The {character[0]} character was found {character[1]} times.")
+        print(f"{character[0]}: {character[1]}")
     print("--- End report ---")
 
 def sort_dictionary(book_text):
@@ -29,7 +30,7 @@ def sort_dictionary(book_text):
     for item in unsorted_list:
         if item[0].isalpha():
             alpha_unsorted_list.append(item)
-    alpha_sorted_list = sorted(alpha_unsorted_list)
+    alpha_sorted_list = sorted(alpha_unsorted_list, key=lambda x: x[1], reverse=True)
     return alpha_sorted_list
 
 
